60bd340e44fd3d6a8d4370aa8dc6d87dd2d4b472
pick a name for me <112062600+gamassss@users.noreply.github.com>
2024-09-18 13:40:44 +0700

    feat: force light mode styles regardless of user preference

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`antialiased ${GeistMono.className}`}
+        className={`antialiased light ${GeistMono.className}`}
       >
         <Navbar></Navbar>
         {children}
